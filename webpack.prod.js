f590180371f2020ed71806bffb8683f3aff594b0
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-06-25 16:44:28 +0300

    terser minimizer for js prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,37 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-const extractCss = require('mini-css-extract-plugin');
-const optimizeCss = require('optimize-css-assets-webpack-plugin');
-
+const ExtractCss = require('mini-css-extract-plugin');
+const OptimizeCss = require('optimize-css-assets-webpack-plugin');
+const TerserMini = require('terser-webpack-plugin');
 module.exports = merge(common, {
-  
+
   mode: 'production',
   output: {
-    filename: "[name].[contentHash].bundle.js",
+    filename: '[name].[contentHash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
 
   },
   optimization: {
     minimizer: [
-      new optimizeCss()
-    ]
+      new OptimizeCss(), new TerserMini(),
+    ],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [extractCss.loader, 'css-loader'],
+        use: [ExtractCss.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [extractCss.loader, 'css-loader', 'sass-loader'],
+        use: [ExtractCss.loader, 'css-loader', 'sass-loader'],
       },
-    ]
+    ],
   },
   plugins: [
-    new extractCss({
+    new ExtractCss({
       filename: 'css/[name].[contentHash].css',
     }),
-  ]
+  ],
 });
